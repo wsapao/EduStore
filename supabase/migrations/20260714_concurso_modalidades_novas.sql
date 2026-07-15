ebd22e24c710f0999f8eb3a6d949961296431e69
Webert Santos <webert.s@live.com>
2026-07-15 10:02:30 -0300

    Revert "feat(concurso): edital atualizado (rev. escola 02/07) + 3 modalidades novas"
    This reverts commit 3c985eb44e7452f7d91efe0bd038ed3278da00b1.

--- supabase/migrations/20260714_concurso_modalidades_novas.sql
+++
@@ -1,9 +0,0 @@
--- Edital nº 01/2026 atualizado (jul/2026): concurso passa a ofertar 8 modalidades
--- (novas: basquete, handebol, ballet). Aplicada em prod via MCP em 14/07/2026
--- como "concurso_modalidades_basquete_handebol_ballet".
-ALTER TABLE public.inscricoes_concurso
-  DROP CONSTRAINT inscricoes_concurso_modalidade_check;
-
-ALTER TABLE public.inscricoes_concurso
-  ADD CONSTRAINT inscricoes_concurso_modalidade_check
-  CHECK (modalidade IN ('futsal','volei','basquete','handebol','judo','ginastica','ballet','natacao'));
